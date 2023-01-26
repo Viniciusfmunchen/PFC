9f5ae6d491afd0d83ca9969bf5bbe78150c690f6
Pedro da Mata Alves <55161573+matinhaalves@users.noreply.github.com>
2023-01-25 21:24:25 -0300

    ajustes

--- soka_animes/resources/views/components/post.blade.php
+++ soka_animes/resources/views/components/post.blade.php
@@ -8,7 +8,7 @@
         <div class="card-footer">
                 <div>
                     @foreach($works as $work)
-                        <span class="badge rounded-pill bg-warning mx-1">{{ $work->name }}</span>
+                        <span class="badge rounded-pill bgphp-warning mx-1">{{ $work->name }}</span>
                     @endforeach
                     @foreach($characters as $character)
                         <a class="badge rounded-pill bg-info mx-1 text-decoration-none" href="{{route('characters.show', $character->id)}}"> {{ $character->name }} </a>
--- soka_animes/resources/views/welcome.blade.php
+++ soka_animes/resources/views/welcome.blade.php
@@ -6,11 +6,12 @@
             <a class="btn btn-primary" href="{{route('works.index')}}">Listagem Obras</a>
         </div>
         <div class="mt-3">
-            <a class="btn btn-primary" href="{{route('characters.index')}}">Listagem dos Personagens</a>
+            <a class="btn btn-warning" href="{{route('characters.index')}}">Listagem dos Personagens</a>
         </div>
         <div class="mt-3">
             <a class="btn btn-primary" href="{{route('genders.index')}}">Listagem dos Generos</a>
         </div>
+       
     </div>
    
 </x-app>
